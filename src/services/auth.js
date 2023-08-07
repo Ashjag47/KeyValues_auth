f0a8d644a51ae5770bb1b67a3945c52062c6cfce
Ashjag47 <ashjagmk@gmail.com>
2023-08-07 22:11:07 +0530

    Feat: add bcrypt to encrypt and store the password.

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,18 +1,22 @@
 const { HTTPError } = require("../utils/errors/HTTPError");
 const { User } = require("../../database/models");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 const generateToken = async (username, password) => {
   const user = await User.findOne({ where: { username } });
   if (!user) {
     throw new HTTPError("INVALID_CREDENTIALS", 400);
   }
-  if (user.password !== password) {
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+  if (!isPasswordCorrect) {
     throw new HTTPError("INVALID_CREDENTIALS", 400);
   }
+
   const token = jwt.sign({ username, password }, "secret", {
     expiresIn: "60m",
   });
+
   const response = {
     status: "success",
     message: "Access token generated successfully.",
--- src/services/users.js
+++ src/services/users.js
@@ -1,9 +1,21 @@
 const { User } = require("../../database/models");
 const { HTTPError } = require("../utils/errors/HTTPError");
+const bcrypt = require("bcryptjs");
 
 const createUser = async (user) => {
   try {
-    const newUser = await User.create(user);
+    const { username, email, password, full_name, age, gender } = user;
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const body = {
+      username,
+      email,
+      password: hashedPassword,
+      full_name,
+      age,
+      gender,
+    };
+    const newUser = await User.create(body);
+
     response = {
       status: "success",
       message: "User successfully registered!",
