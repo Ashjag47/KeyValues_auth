b3e74f235d52cd2b504f4049bbab9585c9072291
Ashjag47 <ashjagmk@gmail.com>
2023-08-07 22:16:30 +0530

    Chore: remove unnecessary comments.

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const cors = require("cors");
 const usersRouter = require("./src/routes/users");
 const authRouter = require("./src/routes/auth");
 
-// const jwt = require("jsonwebtoken");
-
 const app = express();
 const port = 3000;
 
@@ -13,12 +11,6 @@ app.use(express.json());
 
 app.use("/api/token", authRouter);
 app.use("/api", usersRouter);
-
-// app.post("/", (req, res) => {
-//   console.log(req.body.token);
-//   const jwtExample = jwt.verify(req.body.token, "secret");
-//   res.send(jwtExample);
-// });
 
 app.listen(port, () =>
   console.log(`auth app listening on http://localhost:${port}`)
